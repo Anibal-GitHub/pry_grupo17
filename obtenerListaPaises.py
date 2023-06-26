3dee1e6f81bc87427831c17d09e199a0d2ee26f1
Anibal-GitHub <73451620+Anibal-GitHub@users.noreply.github.com>
2023-06-26 20:31:25 -0300

    Update obtenerListaPaises.py

--- obtenerListaPaises.py
+++ obtenerListaPaises.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Cargar los datos del DataSet (archivo CSV llamado "poblacion-mundial.csv")
-data = pd.read_csv("C:/Users/aniba/Downloads/poblacion-mundial.csv")
+data = pd.read_csv("poblacion-mundial.csv")
 
 # Obtener una lista de los países únicos
 lista_paises = data["Entity"].unique()
