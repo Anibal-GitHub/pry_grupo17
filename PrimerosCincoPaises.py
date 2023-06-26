d2d298182544a5a377a2a077cded580797f4c6f2
Anibal-GitHub <anibal_g@live.com.ar>
2023-06-26 20:17:52 -0300

    codigo de primeros 5

---
+++ PrimerosCincoPaises.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+import pandas as pd
+
+# Cargar los datos de la tabla (archivo CSV llamado "poblacion-mundial.csv")
+data = pd.read_csv("C:/Users/aniba/Downloads/poblacion-mundial.csv")
+
+# Mostrar los primeros 5 países
+primeros_paises = data.head(5)
+print(primeros_paises)
